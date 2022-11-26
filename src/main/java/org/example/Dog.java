78be07d61bd48c51bbcadf7fd067f3b4c48ed9ec
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-26 22:33:10 +0300

    Initial commit

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Dog implements Runnable {
+    public void run() {
+        System.out.println("Собака бежит");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.run();
+    }
+}
